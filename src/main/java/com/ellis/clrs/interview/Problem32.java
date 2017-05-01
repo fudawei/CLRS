2f0a60b5d77fa61c9aa132dfa3dfccc5607e3d24
fudawei <fudawei>
2017-05-01 17:12:08 +0800

    /**  * 题目:输入一个整数 n,求从 1 到 n 这 n 个整数的十进制表示中 1 出现的次  * 数。例如输入 12,这些整数中包含 1 的数字有 1,10,11,12, 1 一共出现了 5 次。  */

--- src/main/java/com/ellis/clrs/interview/Problem32.java
+++ src/main/java/com/ellis/clrs/interview/Problem32.java
@@ -4,7 +4,7 @@ package com.ellis.clrs.interview;
  * 题目:输入一个整数 n,求从 1 到 n 这 n 个整数的十进制表示中 1 出现的次
  * 数。例如输入 12,这些整数中包含 1 的数字有 1,10,11,12, 1 一共出现了 5 次。
  */
-public class Problem31 {
+public class Problem32 {
 
   public static void main(String[] args) {
     System.out.println(countOne(257));
